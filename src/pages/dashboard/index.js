d615f9f817f3712a0f33fea0da84f53d2d005dd3
ViniciusOkaeda <61584826+ViniciusOkaeda@users.noreply.github.com>
2023-03-14 10:26:29 -0300

    projeto inicial estruturado

---
+++ src/pages/dashboard/index.js
@@ -0,0 +1,12 @@
+import React, {useState, useEffect} from 'react';
+import './index.css';
+
+const Dashboard = () => {
+
+
+    return(
+        <div></div>
+    );
+}
+
+export default Dashboard;
---
+++ src/routes.js
@@ -0,0 +1,41 @@
+import React, {useEffect } from 'react';
+
+import {
+    Routes,
+    BrowserRouter,
+    Route,
+    Navigate,
+  } from "react-router-dom";
+
+import Dashboard from './pages/dashboard';
+import Login from './pages/login';
+
+
+import PrivateRoute from './utils/privateRoute';
+
+function AllRoutes() {
+
+    const checkRoutes = sessionStorage.getItem("token");
+
+  return (
+
+            <BrowserRouter >
+                <Routes >
+
+                  {!checkRoutes && (
+                  <Route exact path="/" element={<Login />} />)}
+
+                  <Route path="/dashboard" element={
+                        <PrivateRoute>
+                          <Dashboard />
+                        </PrivateRoute> 
+                    } />     
+
+                  <Route path="*" element={<Navigate to ={checkRoutes ? "/dashboard" : "/"} />} />
+
+                </Routes>
+            </BrowserRouter>
+  )
+}
+
+export default AllRoutes;
